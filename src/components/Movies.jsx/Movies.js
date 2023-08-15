1e00b364f379ba1fbeb50b0392aa76462711cedf
Karapuz1982 <121318106+Karapuz1982@users.noreply.github.com>
2023-08-15 19:20:24 +0200

    change#5

--- src/components/Movies.jsx/Movies.js
+++ src/components/Movies.jsx/Movies.js
@@ -49,7 +49,7 @@
 
 
 // export default Movies;
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { searchMovies } from '../../API/api';
 import MovieItem from '../MovieItem.jsx/MovieItem';
@@ -58,30 +58,32 @@ import { MoviesWrapper, Input, Button, MoviesList } from './Movies.styled';
 const Movies = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const searchParams = new URLSearchParams(location.search);
   
-  const initialQuery = searchParams.get('query') || '';
+  const initialQuery = useMemo(() => {
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get('query') || '';
+  }, [location.search]);
 
   const [searchQuery, setSearchQuery] = useState(initialQuery);
   const [searchResults, setSearchResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = useCallback(async () => {
     try {
-      setIsLoading(true); 
-      
+      setIsLoading(true);
+
       const data = await searchMovies(searchQuery);
       setSearchResults(data.results);
 
+      const searchParams = new URLSearchParams(location.search);
       searchParams.set('query', searchQuery);
       navigate(`?${searchParams.toString()}`);
-      
     } catch (error) {
       console.error('Error searching movies:', error);
     } finally {
       setIsLoading(false);
     }
-  }, [searchQuery, navigate, searchParams]);
+  }, [searchQuery, navigate, location.search]);
 
   useEffect(() => {
     if (initialQuery) {
@@ -97,7 +99,7 @@ const Movies = () => {
         onChange={(e) => setSearchQuery(e.target.value)}
       />
       <Button onClick={handleSearch}>Search</Button>
-      {isLoading && <div>Loading...</div>} 
+      {isLoading && <div>Loading...</div>}
       <MoviesList>
         {searchResults.map((movie) => (
           <li key={movie.id}>
