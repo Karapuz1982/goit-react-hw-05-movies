2fbfac836aee379d406e620ba556fa8d75e06861
Karapuz1982 <121318106+Karapuz1982@users.noreply.github.com>
2023-08-17 22:09:29 +0200

    create hw 5

--- src/components/Movies.jsx/Movies.js
+++ src/components/Movies.jsx/Movies.js
@@ -1,44 +1,65 @@
-// import React, { useState } from 'react';
-// import { Link, useLocation, useNavigate  } from 'react-router-dom';
 
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
 // import { searchMovies } from '../../API/api';
 // import MovieItem from '../MovieItem.jsx/MovieItem';
-// import {MoviesWrapper, Input, Button, MoviesList} from './Movies.styled';
+// import { MoviesWrapper, Input, Button, MoviesList } from './Movies.styled';
 
 // const Movies = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
 
-//  const location = useLocation();
-//  const navigate = useNavigate();
+//   const initialQuery = new URLSearchParams(location.search).get('query') || '';
 
-
-//   const searchParams = new URLSearchParams(location.search);
-//   const initialQuery = searchParams.get('query') || '';
-
-//  const [searchQuery, setSearchQuery] = useState(initialQuery);
+//   const [searchQuery, setSearchQuery] = useState(initialQuery);
 //   const [searchResults, setSearchResults] = useState([]);
-  
-//   const handleSearch = async () => {
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const handleSearch = useCallback(async () => {
 //     try {
+//       setIsLoading(true);
+
 //       const data = await searchMovies(searchQuery);
 //       setSearchResults(data.results);
 
-//        searchParams.set('query', searchQuery);
-//      navigate(`?${searchParams.toString()}`);
-     
+//       const searchParams = new URLSearchParams(location.search);
+//       searchParams.set('query', searchQuery);
+//       navigate(`?${searchParams.toString()}`);
 //     } catch (error) {
 //       console.error('Error searching movies:', error);
+//     } finally {
+//       setIsLoading(false);
 //     }
-//   };
+//   }, [searchQuery, navigate, location.search]);
+
+//   // Ефект, який спрацьовує при зміні параметра 'query' в адресній стрічці
+//   useEffect(() => {
+//     if (initialQuery) {
+//       handleSearch();
+//     }
+//   }, [initialQuery, handleSearch]);
+
+//   // Ефект, який спрацьовує при першому рендері сторінки
+//   useEffect(() => {
+//     if (!initialQuery) {
+//       handleSearch();
+//     }
+//   }, [handleSearch, initialQuery]);
 
 //   return (
 //     <MoviesWrapper>
-//       <Input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+//       <Input
+//         type="text"
+//         value={searchQuery}
+//         onChange={(e) => setSearchQuery(e.target.value)}
+//       />
 //       <Button onClick={handleSearch}>Search</Button>
+//       {isLoading && <div>Loading...</div>}
 //       <MoviesList>
 //         {searchResults.map((movie) => (
 //           <li key={movie.id}>
 //             <Link to={`/movies/${movie.id}`}>
-//             <MovieItem movie={movie} />
+//               <MovieItem movie={movie} />
 //             </Link>
 //           </li>
 //         ))}
@@ -47,9 +68,72 @@
 //   );
 // };
 
+// export default Movies;
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+// import { searchMovies } from '../../API/api';
+// import MovieItem from '../MovieItem.jsx/MovieItem';
+// import { MoviesWrapper, Input, Button, MoviesList } from './Movies.styled';
+
+// const Movies = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   const initialQuery = new URLSearchParams(location.search).get('query') || '';
+
+//   const [searchQuery, setSearchQuery] = useState(initialQuery);
+//   const [searchResults, setSearchResults] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const handleSearch = useCallback(async () => {
+//     try {
+//       setIsLoading(true);
+
+//       const data = await searchMovies(searchQuery);
+//       setSearchResults(data.results);
+
+//       const searchParams = new URLSearchParams(location.search);
+//       searchParams.set('query', searchQuery);
+//       navigate(`?${searchParams.toString()}`);
+//     } catch (error) {
+//       console.error('Error searching movies:', error);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   }, [searchQuery, navigate, location.search]);
+// useEffect(() => {
+  
+// }, []);
+//   // Ефект, який спрацьовує при зміні параметра 'query' в адресній стрічці
+//   useEffect(() => {
+//     handleSearch();
+//   }, [searchQuery, handleSearch]);
+
+//   return (
+//     <MoviesWrapper>
+//       <Input
+//         type="text"
+//         value={searchQuery}
+//         onChange={(e) => setSearchQuery(e.target.value)}
+//       />
+//       <Button onClick={handleSearch}>Search</Button>
+//       {isLoading && <div>Loading...</div>}
+//       <MoviesList>
+//         {searchResults.map((movie) => (
+//           <li key={movie.id}>
+//             <Link to={`/movies/${movie.id}`}>
+//               <MovieItem movie={movie} />
+//             </Link>
+//           </li>
+//         ))}
+//       </MoviesList>
+//     </MoviesWrapper>
+//   );
+// };
 
 // export default Movies;
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { searchMovies } from '../../API/api';
 import MovieItem from '../MovieItem.jsx/MovieItem';
@@ -58,47 +142,51 @@ import { MoviesWrapper, Input, Button, MoviesList } from './Movies.styled';
 const Movies = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  
-  const initialQuery = useMemo(() => {
-    const searchParams = new URLSearchParams(location.search);
-    return searchParams.get('query') || '';
-  }, [location.search]);
+
+  const initialQuery = new URLSearchParams(location.search).get('query') || '';
 
   const [searchQuery, setSearchQuery] = useState(initialQuery);
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSearch = useCallback(async () => {
+  const handleSearch = useCallback(async (query) => {
     try {
       setIsLoading(true);
 
-      const data = await searchMovies(searchQuery);
+      const data = await searchMovies(query);
       setSearchResults(data.results);
 
       const searchParams = new URLSearchParams(location.search);
-      searchParams.set('query', searchQuery);
+      searchParams.set('query', query);
       navigate(`?${searchParams.toString()}`);
     } catch (error) {
       console.error('Error searching movies:', error);
     } finally {
       setIsLoading(false);
     }
-  }, [searchQuery, navigate, location.search]);
+  }, [location.search, navigate]);
 
   useEffect(() => {
     if (initialQuery) {
-      handleSearch();
+      handleSearch(initialQuery);
     }
   }, [initialQuery, handleSearch]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch(searchQuery);
+  };
+
   return (
     <MoviesWrapper>
-      <Input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <Button onClick={handleSearch}>Search</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button type="submit">Search</Button>
+      </form>
       {isLoading && <div>Loading...</div>}
       <MoviesList>
         {searchResults.map((movie) => (
